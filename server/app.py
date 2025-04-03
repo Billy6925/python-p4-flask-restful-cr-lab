235112b9b88e2f7bc07b3c778787387bc4d41258
Billy Eskein <billyeskein@gmail.com>
2025-04-03 12:17:51 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -17,11 +17,34 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants= [plant.to_dict() for plant in Plant.query.all()]
+        response= make_response(jsonify(plants),200)
+        return response
+    
+    def post(self):
+        data=request.get_json()
+        if not data or "name" not in data or "image" not in data or "price" not in data:
+            return make_response(jsonify({"error":"Name,Image and Price are required"}))
+        new_plant= Plant(
+            name= data["name"],
+            image= data['image'] ,
+            price= data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response= make_response(jsonify(new_plant.to_dict()),201)
+        return response
+api.add_resource(Plants,'/plants')
 
 class PlantByID(Resource):
-    pass
-        
-
+    def get(self,id):
+        plant= Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({'error':'Plant not found'}),404)
+        response= make_response(jsonify(plant.to_dict()),200)
+        return response
+api.add_resource(PlantByID,'/plants/<int:id>')
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata= MetaData(naming_convention={
+    "fk":"fk_%(table_name)s_%(column_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String)
+    image= db.Column(db.String)
+    price= db.Column(db.Float)
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "image":self.image,
+            "price":self.price
+        }
+
+    def __repr__(self):
+        return f'<Plant {self.id},{self.name},{self.image},{self.price}>'
+
